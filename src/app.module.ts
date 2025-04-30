1c06ef7de9d55d550181c5dd54ae8da40ff4a25c
Anchit Saluja <67328749+hairaat@users.noreply.github.com>
2025-04-30 11:51:09 +0530

    chore: force rebuild by touching app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { VoiceCallModule } from './voice-call/voice-call.module';
+
 @Module({
   imports: [
     VoiceCallModule, // 👈 add this line
   ],
 })
-export class AppModule {}
+export class AppModule {} // 👈 dummy comment to trigger rebuild
