4d290b0119eee4a606f1d13938294ddd8c922807
Anchit Saluja <67328749+hairaat@users.noreply.github.com>
2025-04-30 12:01:28 +0530

    fix: assert MONGO_URI is defined to fix TS2345

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,6 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { VoiceCallModule } from './voice-call/voice-call.module';
 
 @Module({
-  imports: [MongooseModule.forRoot(process.env.MONGO_URI), VoiceCallModule],
+  imports: [MongooseModule.forRoot(process.env.MONGO_URI!), VoiceCallModule],
 })
 export class AppModule {}
